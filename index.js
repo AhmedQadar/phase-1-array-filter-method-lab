ee042a8ef8ee0c27ca4e62e8feb5a5a899dbeea7
ahmedqadar <abdulnet029@gmail.com>
2023-10-09 16:30:42 +0300

    filter-method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (drivers, name) {
+    return drivers.filter(driver => driver.toLocaleLowerCase() === name.toLocaleLowerCase());
+}
+
+function fuzzyMatch (drivers, name) {
+    let fuzzyDrivers = [];
+    fuzzyDrivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby');
+    return drivers.filter(drivers => drivers.slice(0,2).toLowerCase() === name.toLowerCase());
+}
+
+function matchName (drivers, name ) {
+    return drivers.filter(drivers => drivers.name === name);
+}
+
